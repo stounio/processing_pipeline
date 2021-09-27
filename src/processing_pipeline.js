b64ec744c056afc5db462be7ba4d7a9568feded3
Guillaume Pichot <guillaume.pichot@metapack.com>
2021-09-27 12:31:18 +0100

    No items are deleted when the criteria does not match

--- src/processing_pipeline.js
+++ src/processing_pipeline.js
@@ -1,3 +1,7 @@
-const deleteItemsByCriteria = async (criteria, callerId) => { throw new Error() };
+const deleteItemsByCriteria = async (criteria, callerId) => criteria ? proceed() : reject();
+
+const proceed = () => 0;
+
+const reject = () => { throw new Error() };
 
 module.exports = { deleteItemsByCriteria };
--- test/processing_pipeline_test.js
+++ test/processing_pipeline_test.js
@@ -10,4 +10,9 @@ describe('Delete items by criteria', () => {
   it('should reject the call by default', async () => {
     expect(deleteItemsByCriteria()).to.be.rejected;
   });
+
+  it('should delete zero one items when the criteria does not match', async () => {
+    const result = await deleteItemsByCriteria('NO_MATCH');
+    expect(result).to.eq(0);
+  });
 });
